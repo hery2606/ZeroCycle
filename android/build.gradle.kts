20f7f040455c08eaf322bf61b88bad88548675d6
Heriaristaa <2300016151@webmail.uad.ac.id>
2025-07-11 21:20:28 +0700

    yeah

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,9 +1,14 @@
-allprojects {
+buildscript {
     repositories {
         google()
         mavenCentral()
     }
+    
+    dependencies {
+        classpath("com.google.gms:google-services:4.4.0")
+    }
 }
+
 
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
@@ -12,6 +17,7 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
